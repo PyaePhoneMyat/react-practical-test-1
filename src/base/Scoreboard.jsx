b51359be2a437fc2a6530c68dc6d548091ad9495
Pyae Phone Myat <pyaephonemyat2004@gmail.com>
2024-01-06 12:24:20 +0630

    scoreboard

--- src/base/Scoreboard.jsx
+++ src/base/Scoreboard.jsx
@@ -1,6 +1,6 @@
 const Scoreboard = ({ totalScore, highScore, selectedMode }) => {
   return (
-    <div className='border border-red-700 grid grid-cols-3 text-center font-medium text-3xl px-10'>
+    <div className='grid grid-cols-3 text-center font-medium text-3xl px-10'>
       <div>Your score:</div>
       {totalScore < 0 ? (
         <div className='text-green-500'>0</div>
